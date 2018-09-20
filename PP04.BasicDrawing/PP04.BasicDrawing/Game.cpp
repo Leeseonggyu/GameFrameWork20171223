9915709036ba0d016ff71a0a241a1e31f23796ae
이성규 <seong9945@naver.com>
2018-09-20 22:12:45 +0900

    9월 20일

--- PP04.BasicDrawing/PP04.BasicDrawing/Game.cpp
+++ PP04.BasicDrawing/PP04.BasicDrawing/Game.cpp
@@ -3,25 +3,24 @@
 bool Game::init(const char*title, int xpos, int ypos, int width,
 	int height, bool fullscreen)
 {
-	SDL_Surface*pTempSurface = SDL_LoadBMP("asset/ rider.bmp");
-	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-
-	SDL_FreeSurface(pTempSurface);
-	SDL_QueryTexture(m_pTexture, NULL, NULL,
-		&m_sourceRectangle.w, &m_sourceRectangle.h);
-
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		g_pWindow = SDL_CreateWindow("PPO4. BasicGame",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
-		if (g_pWindow != 0) {
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+		if (m_pWindow != 0) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
+
+		m_bRunning = true;
+
+		SDL_Surface*pTempSurface = SDL_LoadBMP("asset/ rider.bmp");
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+		SDL_FreeSurface(pTempSurface);
+		SDL_QueryTexture(m_pTexture, NULL, NULL,
+			&m_sourceRectangle.w, &m_sourceRectangle.h);
 	}
 	else {
-		return 1;
+		return false;
 	}
 	return true;
 }
