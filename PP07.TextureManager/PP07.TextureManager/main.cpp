af49a1284b00e469f0bd7d7d4950d07a3a2a4f61
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-10-12 11:57:51 +0900

    PP07 10월 11일

---
+++ PP07.TextureManager/PP07.TextureManager/main.cpp
@@ -0,0 +1,18 @@
+#include"Game.h"
+
+Game*g_game = 0;
+
+int main(int argc, char*argv[])
+{
+	g_game = new Game();
+	g_game->init("Chapter 2", 640, 480, 640, 480, true);
+
+	while (g_game->running())
+	{
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
+	}
+	g_game->clean();
+	return 0;
+}
